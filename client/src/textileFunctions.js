42acb75e195dd9b6f2335b4fd3ee55ebdf67a985
korabs-x <ko.abstreiter@gmail.com>
2022-04-23 17:29:15 +0200

    changes

---
+++ client/src/textileFunctions.js
@@ -0,0 +1,36 @@
+import {Buckets} from '@textile/hub'
+import {Buffer} from 'buffer'
+
+const keyInfo = {
+    key: process.env.REACT_APP_KEY_TEXTILEBUCKETS,
+    secret: process.env.REACT_APP_SECRET_TEXTILEBUCKETS
+}
+
+async function getBucketClient() {
+    const buckets = await Buckets.withKeyInfo(keyInfo)
+    return buckets;
+}
+
+async function getOrCreateBucket(buckets, bucketName) {
+    const {root,} = await buckets.getOrCreate(bucketName)
+    if (!root) throw new Error('bucket not created')
+    const bucketKey = root.key
+    return bucketKey;
+}
+
+export async function pushTextToIpfs(text) {
+    const bucketsX = await getBucketClient();
+    //console.log(bucketsX);
+    let bucketKeyX = await getOrCreateBucket(bucketsX, 'testbucket');
+    //console.log(bucketKeyX);
+    const fileX = {
+        path: '/index.html',
+        //content: text//new Uint8Array(Buffer.from(text))
+        content: Buffer.from(text)
+    }
+    //console.log(fileX);
+    const links = await bucketsX.pushPath(bucketKeyX, 'text.txt', fileX)//, { root })
+    console.log(`https://hub.textile.io${links.path.path}`);
+    return links.path.path;
+}
+
